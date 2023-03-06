58fe3c5f56aca5f809df4c68446379c19389f060
ahmet <a.duser@lancaster.ac.uk>
2023-03-06 16:52:04 +0100

    start of the application moved to proper directory

--- src/main/java/com/lancaster/SymptomSubtitleApplication.java
+++ src/main/java/com/lancaster/SymptomSubtitleApplication.java
@@ -1,4 +1,4 @@
-package com.lancaster.symptomsubtitle;
+package com.lancaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
